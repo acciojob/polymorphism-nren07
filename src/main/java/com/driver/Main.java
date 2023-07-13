9d50f785b33bc1f483827cabba5d72912be785a5
nren07 <imnren07@gmail.com>
2023-07-13 09:37:21 +0530

    product function implimented

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,25 +5,25 @@ import java.util.Scanner;
 public class Main {
     public static class Product{
         public int product(int a, int b){
-            return a+b;
+            return a*b;
         }
         public int product( int a, int b, int c){
-            return a+b+c;
+            return a*b*c;
         }
         public double product( double x, double y){
-            return x+y;
+            return x*y;
         }
     }
 
     public static void main(String[] args) {
         Scanner sc=new Scanner( System.in);
-        System.out.println("enter integer a, b, c");
+//        System.out.println("enter integer a, b, c");
 
         int a=sc.nextInt();
         int b=sc.nextInt();
         int c=sc.nextInt();
 
-        System.out.println("enter double data type x, y");
+//        System.out.println("enter double data type x, y");
         double x=sc.nextDouble();
         double y= sc.nextDouble();
 
@@ -33,8 +33,7 @@ public class Main {
         int b1= p.product(a,b,c); // calling method having args int a,int b, int c
         double c1=p.product(x,y); // calling method having arg double x, double y
 
-        System.out.println("Sum of a+b:-> "+a1+"\n"+"Sum of Integer a+b+c:-> "+b1+"\n"+"Sum of double data type x+y :-> "+c1);
-
+//        System.out.println("Sum of a+b:-> "+a1+"\n"+"Sum of Integer a+b+c:-> "+b1+"\n"+"Sum of double data type x+y :-> "+c1);
 
     }
 }
